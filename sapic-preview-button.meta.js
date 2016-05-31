ced70450183a71713a8371dca1384ab38ef7d139
The Oddball <oddball@oddball.tf>
2016-05-30 20:13:20 -0700

    v2.1
    Added preview button to Inventories
    Fixed bug inv preview button

--- sapic-preview-button.meta.js
+++ sapic-preview-button.meta.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Preview Steam Background on Sapic
 // @namespace    http://steamcommunity.com/id/theoddball/
-// @version      1.1
+// @version      2.1
 // @description  Adds a Sapic preview button to Steam Market background listings
 // @author       The Oddball
 // @include      /http:\/\/steamcommunity\.com\/market\/listings\/753\/.*/
+// @include      /http:\/\/steamcommunity\.com\/id\/.*\/inventory\/.*/
+// @include      /https:\/\/steamcommunity\.com\/id\/.*\/inventory\/.*/
 // @include      /https:\/\/steamcommunity\.com\/market\/listings\/753\/.*/
 // @grant        none
 // @downloadURL  https://theoddball.github.io/sapic-preview-button.user.js
