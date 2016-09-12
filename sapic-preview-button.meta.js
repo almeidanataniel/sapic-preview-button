7c1089f9d1f353e0abe7e6f9aad9f8ef5b3917f1
The Oddball <oddball@oddball.tf>
2016-09-12 10:58:56 -0700

    v2.6
    Added a button to SteamCardExchange game pages. (Steam ones wont try to
    load here and vice versa)
    Buttons only try to load when you're on their respective pages
    Fixed some spacing issues with profile settings buttons

--- sapic-preview-button.meta.js
+++ sapic-preview-button.meta.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Preview Steam Background on Sapic
 // @namespace    http://oddball.tf
-// @version      2.5
+// @version      2.6
 // @description  Adds a Sapic preview button to Steam Market background listings
 // @author       The Oddball
 // @include      /http:\/\/steamcommunity\.com\/.*/
 // @include      /https:\/\/steamcommunity\.com\/.*/
+// @include      /http:\/\/www.steamcardexchange.net\/.*/
+// @include      /https:\/\/www.steamcardexchange.net\/.*/
 // @grant        none
 // @downloadURL  https://github.com/theoddball/sapic-preview-button/raw/master/sapic-preview-button.user.js
 // @updateURL    https://github.com/theoddball/sapic-preview-button/raw/master/sapic-preview-button.meta.js
--- sapic-preview-button.user.js
+++ sapic-preview-button.user.js
@@ -1,18 +1,39 @@
 // ==UserScript==
 // @name         Preview Steam Background on Sapic
 // @namespace    http://oddball.tf
-// @version      2.5
+// @version      2.6
 // @description  Adds a Sapic preview button to Steam Market background listings
 // @author       The Oddball
 // @include      /http:\/\/steamcommunity\.com\/.*/
 // @include      /https:\/\/steamcommunity\.com\/.*/
+// @include      /http:\/\/www.steamcardexchange.net\/.*/
+// @include      /https:\/\/www.steamcardexchange.net\/.*/
 // @grant        none
 // @downloadURL  https://github.com/theoddball/sapic-preview-button/raw/master/sapic-preview-button.user.js
 // @updateURL    https://github.com/theoddball/sapic-preview-button/raw/master/sapic-preview-button.meta.js
 // ==/UserScript==
-var $ = $J;
+function load(){
+    var hostname = window.location.hostname;
+    var steam = /steamcommunity/.test(hostname);
+    var sce = /steamcardexchange/.test(hostname);
+    var href = window.location.href;
+    if(steam){
+
+        if(/\/753\//.test(href)){
+            scmSapicButton();
+        } else if(/\/inventory\//.test(href)){
+            invSapicButton();
+        } else if(/\/edit/.test(href)){
+            settingsSapicButton();
+        }
+    }
+    if(sce && /gamepage-appid/.test(href)){
+        sceSapicButton();
+    }
+}
 
 function scmSapicButton() {
+    var $ = $J;
     var viewFullButton = $("#largeiteminfo_item_actions").find("a");
     if (viewFullButton.length) {
         var href = viewFullButton.attr('href');
@@ -23,6 +44,7 @@ function scmSapicButton() {
     }
 }
 function invSapicButton(){
+    var $ = $J;
     setInterval(function() {
         var itemActions = $(".inventory_iteminfo").find(".item_desc_content").find(".item_desc_description").find("#iteminfo1_item_actions");
         var viewFullButton = $(itemActions).find("a").first();
@@ -47,17 +69,23 @@ function invSapicButton(){
     }, 200);
 }
 function settingsSapicButton(){
+    var $ = $J;
     setInterval(function() {
-        var url = $("#profile_background_current_image").attr("src");
+        var url = $('#profile_background_current_image').attr("src");
         var sapicButton = $(".settings_sapic_button");
         url = url.replace("?size=140x90&v=2", "");
 
-        $(".background_selector_launch_area").find(".btn_grey_white_innerfade.btn_small").first().before('<a class="settings_sapic_button btn_small btn_grey_white_innerfade" target="_blank" href="https://steam.design/#' + url + '"><span>Preview on Sapic</span></a>');
+        $(".background_selector_launch_area").find(".btn_grey_white_innerfade.btn_small").first().before('<a class="settings_sapic_button btn_small btn_grey_white_innerfade" style="margin-right: 10px;" target="_blank" href="https://steam.design/#' + url + '"><span>Preview on Sapic</span></a>');
         sapicButton.remove();
     }, 200);
 }
-settingsSapicButton();
-scmSapicButton();
-invSapicButton();
-
-
+function sceSapicButton(){
+    $('.showcase-element').find('.button-blue.preview').each(function (){
+        var href = $(this).parent().parent().find('.element-link-right').attr("href");
+        $(this).parent().find('.button-blue.preview').before('<a class="button-blue" style="bottom: 5px;" target="_blank" href="https://steam.design/#' + href + '"><span>Preview on Sapic</span></a>');
+    });
+    $('.button-blue.market').each(function (){
+        $(this).css('top', '175px');
+    });
+}
+load();
